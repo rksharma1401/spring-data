683d6dcb3a1ba212f940875fba14221f12f389a5
Ravikant Sharma <rksharma1401@hotmail.com>
2023-05-25 15:13:32 +0530

    Create StoreDataApi.java

---
+++ src/main/java/com/learn/storage/rest/StoreDataApi.java
@@ -0,0 +1,46 @@
+package com.learn.storage.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.storage.dao.StoreDataDao;
+import com.learn.storage.model.StoreData;
+
+@RestController
+@RequestMapping("storeDataApi")
+@CrossOrigin
+public class StoreDataApi {
+
+	@Autowired
+	private StoreDataDao dao;
+
+	@PostMapping("createData")
+	public ResponseEntity<StoreData> createData(@RequestParam String title, @RequestParam String text) {
+
+		StoreData d = new StoreData(title, text);
+		StoreData save = dao.save(d);
+
+		return ResponseEntity.ok(save);
+
+	}
+
+	@GetMapping("getAll")
+	public @ResponseBody List<StoreData> getAllData() {
+
+		List<StoreData> result = new ArrayList<>();
+		dao.findAll().forEach(result::add);
+
+		return result;
+
+	}
+}
