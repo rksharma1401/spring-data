a7921d944c7c4dd845548fea071e646206725e80
Ravikant Sharma <rksharma1401@hotmail.com>
2023-05-27 14:40:52 +0530

    Create ParseApiService.java

---
+++ src/main/java/com/learn/storage/service/ParseApiService.java
@@ -0,0 +1,38 @@
+package com.learn.storage.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.learn.storage.model.ListStoreMessage;
+import com.learn.storage.model.StoreMessage;
+
+@Service
+public class ParseApiService {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${parseUrl}")
+	private String url;
+
+	public void save(StoreMessage message) {
+		restTemplate.postForEntity(url, message, StoreMessage.class);
+	}
+
+	public ListStoreMessage getAllData() {
+		String message = "where={\"toDelete\":\"yes\"}";
+
+		return restTemplate.getForObject(url, ListStoreMessage.class, message);
+
+	}
+
+	public ListStoreMessage getByName(String key) {
+		String message = "where= {\"name\":\"" + key + ":}";
+
+		return restTemplate.getForObject(url, ListStoreMessage.class, message);
+
+	}
+
+}
