c48e20f357802e3a815b585ef126ead0a84112a8
ravikant <ravikant>
2023-07-08 18:16:43 +0530

    add test and jars

--- src/main/java/com/learn/storage/WebSocketController.java
+++ src/main/java/com/learn/storage/WebSocketController.java
@@ -3,6 +3,7 @@ package com.learn.storage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.learn.storage.model.Response;
 import com.learn.storage.model.StoreMessage;
@@ -12,6 +13,7 @@ public class WebSocketController {
 	
 	@MessageMapping("/message")
 	@SendTo("/topic/response")
+	@CrossOrigin
 	public Response send(StoreMessage message) throws Exception { 
 		System.out.println("Received request for:- "+message.toString());
 	    return new Response("successfully got from :-  "+message.name());
--- src/main/java/com/learn/storage/config/WebSocketConfig.java
+++ src/main/java/com/learn/storage/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/web-socket-endpoint").withSockJS();
+        registry.addEndpoint("/web-socket-endpoint").setAllowedOriginPatterns("*") .withSockJS();
     }
 
 }
