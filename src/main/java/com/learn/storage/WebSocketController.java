3d33055f6b91e53ebe40bf1437d3aad8cc7d4ba8
Ravikant Sharma <rksharma1401@hotmail.com>
2023-07-12 17:47:40 +0530

    Update WebSocketController.java

--- src/main/java/com/learn/storage/WebSocketController.java
+++ src/main/java/com/learn/storage/WebSocketController.java
@@ -1,4 +1,4 @@
-package com.learn.storage;
+package com.learn.storage.rest;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,13 +10,22 @@ import com.learn.storage.model.StoreMessage;
 
 @Controller
 public class WebSocketController {
-    // Handles messages from /app/message. (Note the Spring adds the /app prefix for us).
+
+	// Handles messages from /app/message. (Note the Spring adds the /app prefix for us).
 	@MessageMapping("/message")
-    // Sends the return value of this method to /topic/messages
+	// Sends the return value of this method to /topic/messages
 	@SendTo("/topic/response")
 	@CrossOrigin
-	public Response send(StoreMessage message) throws Exception { 
-		System.out.println("Received request for:- "+message.toString());
-	    return new Response("successfully got from :-  "+message.name());
+	public Response send(StoreMessage message) throws Exception {
+		System.out.println("Received request for:- " + message.toString());
+		return new Response("successfully got from :-  " + message.name());
+	}
+
+	@MessageMapping("/stringMessage")
+	@SendTo("/topic/stringResponse")
+	@CrossOrigin
+	public String sendString(String message) {
+		System.out.println("sendString->Received request for:- " + message);
+		return ("successfully got from :-  " + message);
 	}
 }
